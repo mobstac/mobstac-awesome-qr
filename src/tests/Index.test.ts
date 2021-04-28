1342a93e02a6825485d2786f3e6bc825730a9ae1
hritik <hritik.aggarwal31@gmail.com>
2021-04-28 16:55:21 +0530

    coverage update

--- src/tests/Index.test.ts
+++ src/tests/Index.test.ts
@@ -24,13 +24,14 @@ const config = {
     backgroundImage: 'https://i.pinimg.com/474x/d4/48/2b/d4482ba4e7ebdbff7b8ba73e7d39aceb.jpg',
     backgroundColor: 'white',
     canvasType: CanvasType.SVG,
-    eyeFrameShape: EyeFrameShape.ROUNDED,
-    eyeBallShape: EyeBallShape.ROUNDED,
+    eyeFrameShape: EyeFrameShape.SQUARE,
+    eyeBallShape: EyeBallShape.SQUARE,
+    dataPattern: DataPattern.SQUARE,
    colorDark: 'yellow',
    colorLight: '#4494fc',
     dotScale: 1,
-     gradientType: GradientType.HORIZONTAL,
-    frameStyle: QRCodeFrame.BALLOON_TOP,
+     gradientType: GradientType.NONE,
+    frameStyle: QRCodeFrame.NONE,
     frameColor: 'blue',
     frameText: 'HEY QR',
     logoMargin: 20,
@@ -47,13 +48,14 @@ const config2 = {
    // backgroundImage: 'https://s3.amazonaws.com/beaconstac-content-qa/1593/9653e5dae58849b9bf523e27142f875e',
     backgroundColor: 'white',
     canvasType: CanvasType.SVG,
-    eyeFrameShape: EyeFrameShape.ROUNDED,
-    eyeBallShape: EyeBallShape.ROUNDED,
+    eyeFrameShape: EyeFrameShape.CIRCLE,
+    eyeBallShape: EyeBallShape.CIRCLE,
+    dataPattern: DataPattern.CIRCLE,
    colorDark: 'yellow',
    colorLight: '#4494fc',
     dotScale: 1,
-     gradientType: GradientType.HORIZONTAL,
-    frameStyle: QRCodeFrame.BANNER_BOTTOM,
+     gradientType: GradientType.LINEAR,
+    frameStyle: QRCodeFrame.BOX_BOTTOM,
     frameColor: 'blue',
     frameText: 'HEY QR',
     logoMargin: 20,
@@ -73,11 +75,12 @@ const config3 = {
     canvasType: CanvasType.SVG,
     eyeFrameShape: EyeFrameShape.ROUNDED,
     eyeBallShape: EyeBallShape.ROUNDED,
+    dataPattern: DataPattern.KITE,
    colorDark: 'yellow',
    colorLight: '#4494fc',
     dotScale: 1,
-     gradientType: GradientType.HORIZONTAL,
-    frameStyle: QRCodeFrame.BANNER_BOTTOM,
+     gradientType: GradientType.RADIAL,
+    frameStyle: QRCodeFrame.BOX_TOP,
     frameColor: 'blue',
     frameText: 'HEY QR',
     logoMargin: 20,
@@ -94,8 +97,9 @@ const config4 = {
    // backgroundImage: 'https://s3.amazonaws.com/beaconstac-content-qa/1593/9653e5dae58849b9bf523e27142f875e',
     backgroundColor: 'white',
     canvasType: CanvasType.SVG,
-    eyeFrameShape: EyeFrameShape.ROUNDED,
+    eyeFrameShape: EyeFrameShape.RIGHT_LEAF,
     eyeBallShape: EyeBallShape.ROUNDED,
+    dataPattern: DataPattern.LEFT_DIAMOND,
    colorDark: 'yellow',
    colorLight: '#4494fc',
     dotScale: 1,
@@ -110,6 +114,100 @@ const config4 = {
     isVCard: false,
     useCanvas: false,
     useOpacity: true,
+};
+const config5 = {
+    text: 'https://www.beaconstac.com',
+   // backgroundImage: 'https://s3.amazonaws.com/beaconstac-content-qa/1593/9653e5dae58849b9bf523e27142f875e',
+    backgroundColor: 'white',
+    canvasType: CanvasType.SVG,
+    eyeFrameShape: EyeFrameShape.LEFT_LEAF,
+    eyeBallShape: EyeBallShape.LEFT_LEAF,
+   colorDark: 'yellow',
+   dataPattern: DataPattern.LEFT_DIAMOND,
+   colorLight: '#4494fc',
+    dotScale: 1,
+    gradientType: GradientType.HORIZONTAL,
+    frameColor: 'blue',
+    frameText: 'HEY QR frame',
+    frameStyle: QRCodeFrame.BANNER_TOP,
+    logoMargin: 20,
+    logoScale: 0.25,
+    margin: 80,
+    typeNumber: 5,
+    size: 256,
+    isVCard: true,
+    useCanvas: true,
+    useOpacity: true,
+};
+const config6 = {
+    text: 'https://www.beaconstac.com',
+   // backgroundImage: 'https://s3.amazonaws.com/beaconstac-content-qa/1593/9653e5dae58849b9bf523e27142f875e',
+    backgroundColor: 'white',
+    canvasType: CanvasType.SVG,
+    eyeFrameShape: EyeFrameShape.RIGHT_LEAF,
+    eyeBallShape: EyeBallShape.RIGHT_LEAF,
+    dataPattern: DataPattern.RIGHT_DIAMOND,
+   colorDark: 'yellow',
+   colorLight: '#4494fc',
+    dotScale: 1,
+    gradientType: GradientType.VERTICAL,
+    frameColor: 'blue',
+    frameText: 'HEY QR frame',
+    frameStyle: QRCodeFrame.BANNER_BOTTOM,
+    logoMargin: 20,
+    logoScale: 0.25,
+    margin: 80,
+    typeNumber: 5,
+    size: 256,
+    isVCard: false,
+    useCanvas: false,
+    useOpacity: false,
+};
+const config7 = {
+    text: 'https://s3.amazonaws.com/beaconstac-content-qa/1593/9653e5dae58849b9bf523e27142f875e',
+   // backgroundImage: 'https://s3.amazonaws.com/beaconstac-content-qa/1593/9653e5dae58849b9bf523e27142f875e',
+    backgroundColor: 'white',
+    canvasType: CanvasType.SVG,
+    eyeFrameShape: EyeFrameShape.RIGHT_LEAF,
+    eyeBallShape: EyeBallShape.LEFT_DIAMOND,
+   colorDark: 'yellow',
+   colorLight: '#4494fc',
+    dotScale: 1,
+    gradientType: GradientType.HORIZONTAL,
+    frameColor: 'blue',
+    frameText: 'HEY QR frame',
+    logoMargin: 20,
+    logoScale: 0.25,
+    margin: 80,
+    frameStyle: QRCodeFrame.BALLOON_BOTTOM,
+    typeNumber: 5,
+    size: 256,
+    isVCard: false,
+    useCanvas: false,
+    useOpacity: false,
+};
+const config8 = {
+    text: 'https://www.google.com/search?q=google+image&sxsrf=ALeKk01HdEjd-1kgx0opDH4z57mKAKdSfg:1619608675472&source=lnms&tbm=isch&sa=X&ved=2ahUKEwja3Zq-6KDwAhVDjOYKHeseBpQQ_AUoAXoECAEQAw&biw=1853&bih=949#imgrc=NaNXoifrEY1VZM',
+   // backgroundImage: 'https://s3.amazonaws.com/beaconstac-content-qa/1593/9653e5dae58849b9bf523e27142f875e',
+    backgroundColor: 'white',
+    canvasType: CanvasType.SVG,
+    eyeFrameShape: EyeFrameShape.RIGHT_LEAF,
+    eyeBallShape: EyeBallShape.RIGHT_DIAMOND,
+   colorDark: 'yellow',
+   colorLight: '#4494fc',
+    dotScale: 1,
+    gradientType: GradientType.HORIZONTAL,
+    frameColor: 'blue',
+    frameText: 'HEY QR frame',
+    logoMargin: 20,
+    logoScale: 0.25,
+    frameStyle: QRCodeFrame.BALLOON_TOP,
+    margin: 80,
+    typeNumber: 5,
+    size: 256,
+    isVCard: false,
+    useCanvas: false,
+    useOpacity: false,
 };
 
 describe('QR code tests', () => {
@@ -126,7 +224,7 @@ describe('QR code tests', () => {
     //     });
     // });
 
-    it('Main test QR', done => {
+    it('Main test QR 1', done => {
         const qrCodeGenerator = new QRCodeBuilder(config);
 
         qrCodeGenerator.build(CanvasType.PNG).then(qrCode => {
@@ -246,6 +344,130 @@ describe('QR code tests', () => {
             // done();
         }).catch(err => {
             expect(err).to.equal('Frame text length exceeded');
+            done();
+        });
+    });
+    it('Main test QR 5', done => {
+        const qrCodeGenerator = new QRCodeBuilder(config5);
+
+        qrCodeGenerator.build(CanvasType.PNG).then(qrCode => {
+            
+            const fs = require('fs');
+             
+            const dataUrl = qrCode.canvas.toDataURL('image/png');
+            const matches: any = dataUrl.match(
+                    /^data:([A-Za-z-+\/]+);base64,(.+)$/
+                  ),
+                response: any  ={};
+            response.type = matches[1];
+            response.data = Buffer.from(matches[2], "base64");
+            const decodedImg = response;
+            const imageBuffer = decodedImg.data;
+            const extension ='png';
+            const fileName = '/test5' + "." + extension;
+            fs.writeFileSync(__dirname+fileName, imageBuffer);
+            // console.log(dataUrl.substr(0,200));
+            // fs.writeFileSync(__dirname + '/test.' + CanvasType.SVG.toLowerCase(), qrCode.toBuffer());
+            done();
+            
+            
+            // const fs = require('fs');
+            // fs.writeFileSync(__dirname + '/test.' + CanvasType.SVG.toLowerCase(), qrCode.toBuffer());
+            // done();
+        }).catch(err => {
+            done();
+        });
+    });
+    it('Main test QR 6', done => {
+        const qrCodeGenerator = new QRCodeBuilder(config6);
+
+        qrCodeGenerator.build(CanvasType.PNG).then(qrCode => {
+            
+            const fs = require('fs');
+             
+            const dataUrl = qrCode.canvas.toDataURL('image/png');
+            const matches: any = dataUrl.match(
+                    /^data:([A-Za-z-+\/]+);base64,(.+)$/
+                  ),
+                response: any  ={};
+            response.type = matches[1];
+            response.data = Buffer.from(matches[2], "base64");
+            const decodedImg = response;
+            const imageBuffer = decodedImg.data;
+            const extension ='png';
+            const fileName = '/test6' + "." + extension;
+            fs.writeFileSync(__dirname+fileName, imageBuffer);
+            // console.log(dataUrl.substr(0,200));
+            // fs.writeFileSync(__dirname + '/test.' + CanvasType.SVG.toLowerCase(), qrCode.toBuffer());
+            done();
+            
+            
+            // const fs = require('fs');
+            // fs.writeFileSync(__dirname + '/test.' + CanvasType.SVG.toLowerCase(), qrCode.toBuffer());
+            // done();
+        }).catch(err => {
+            done();
+        });
+    });
+    it('Main test QR 7', done => {
+        const qrCodeGenerator = new QRCodeBuilder(config7);
+
+        qrCodeGenerator.build(CanvasType.PNG).then(qrCode => {
+            
+            const fs = require('fs');
+             
+            const dataUrl = qrCode.canvas.toDataURL('image/png');
+            const matches: any = dataUrl.match(
+                    /^data:([A-Za-z-+\/]+);base64,(.+)$/
+                  ),
+                response: any  ={};
+            response.type = matches[1];
+            response.data = Buffer.from(matches[2], "base64");
+            const decodedImg = response;
+            const imageBuffer = decodedImg.data;
+            const extension ='png';
+            const fileName = '/test7' + "." + extension;
+            fs.writeFileSync(__dirname+fileName, imageBuffer);
+            // console.log(dataUrl.substr(0,200));
+            // fs.writeFileSync(__dirname + '/test.' + CanvasType.SVG.toLowerCase(), qrCode.toBuffer());
+            done();
+            
+            
+            // const fs = require('fs');
+            // fs.writeFileSync(__dirname + '/test.' + CanvasType.SVG.toLowerCase(), qrCode.toBuffer());
+            // done();
+        }).catch(err => {
+            done();
+        });
+    });
+    it('Main test QR 8 very large text', done => {
+        const qrCodeGenerator = new QRCodeBuilder(config8);
+
+        qrCodeGenerator.build(CanvasType.PNG).then(qrCode => {
+            
+            const fs = require('fs');
+             
+            const dataUrl = qrCode.canvas.toDataURL('image/png');
+            const matches: any = dataUrl.match(
+                    /^data:([A-Za-z-+\/]+);base64,(.+)$/
+                  ),
+                response: any  ={};
+            response.type = matches[1];
+            response.data = Buffer.from(matches[2], "base64");
+            const decodedImg = response;
+            const imageBuffer = decodedImg.data;
+            const extension ='png';
+            const fileName = '/test8' + "." + extension;
+            fs.writeFileSync(__dirname+fileName, imageBuffer);
+            // console.log(dataUrl.substr(0,200));
+            // fs.writeFileSync(__dirname + '/test.' + CanvasType.SVG.toLowerCase(), qrCode.toBuffer());
+            done();
+            
+            
+            // const fs = require('fs');
+            // fs.writeFileSync(__dirname + '/test.' + CanvasType.SVG.toLowerCase(), qrCode.toBuffer());
+            // done();
+        }).catch(err => {
             done();
         });
     });
