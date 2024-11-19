786f124ab12416f7886662f756a37a5ab658a5b5
Aditya Trivedi <104889221+aditya-beaconstac@users.noreply.github.com>
2024-11-19 12:53:27 +0530

    Minor Fix for empty state

--- src/index.ts
+++ src/index.ts
@@ -42,10 +42,10 @@ export class QRCodeBuilder {
         }
         if (this.config.frameText && this.config.frameText.length > 30) {
             // Calculations for multiline text
-            const text = this.config.frameText;
-            const textLinesLength = text.split('\n').length;
-            const textLineMaxLength = text.split('\n').map(value => value.trim())
-                .reduce((max, line) => Math.max(max, line.length), 0);
+            const text = this.config.frameText
+            const textLinesLength = text.length ? text.split('\n').length : 0;
+            const textLineMaxLength = this.config.frameText? this.config.frameText.split('\n').map(value => value.trim())
+                .reduce((max, line) => Math.max(max, line.length), 0) : 7;
             if ( textLineMaxLength > 30 ) {
                 return Promise.reject('Frame text length exceeded');
             }
