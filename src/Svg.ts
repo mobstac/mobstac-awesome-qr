2221e84d1e68830dad0d989d2aadb4f6dddd95a2
Rohit Shakya <rohit.s@beaconstac.com>
2024-12-06 13:25:41 +0530

    changes added

--- src/Svg.ts
+++ src/Svg.ts
@@ -256,7 +256,7 @@ export class SVGDrawing {
                     canvas.width(null);
                     // @ts-ignore
                     canvas.height(null);
-                    if(frameStyle === QRCodeFrame.CIRCULAR  && this.config.stickerImageName === ''){
+                    if(frameStyle === QRCodeFrame.CIRCULAR){
                         //@ts-ignore
                         canvas.viewbox(0, 0, canvasWidth + 190, canvasWidth + 190)
                     }
